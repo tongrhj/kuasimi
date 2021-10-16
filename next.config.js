71a5e7fa7973619c8f06f872eac78565917a13be
Jared Tong <tongrhj@users.noreply.github.com>
2021-10-16 17:13:50 +0800

    Redirect legacy Sapper paths to NextJS

--- next.config.js
+++ next.config.js
@@ -1,16 +1,44 @@
 module.exports = {
   async redirects() {
-    return [
-      // {
-      //   source: '/dpo/posb',
-      //   destination: '/dpo/dbs',
-      //   permanent: true
-      // },
-      // {
-      //   source: '/:path(^\w+$)',
-      //   destination: '/dpo/:path',
-      //   permanent: false
-      // }
-    ]
+    const redirectSapperToNextJSPaths = [
+      'cimb',
+      'scb',
+      'dbs',
+      'citibank',
+      'posb',
+      'hsbc',
+      'maybank',
+      'ocbc',
+      'uob',
+      'lazada',
+      'carousell',
+      'grab',
+      'qoo10',
+      'shopee',
+      'zalora',
+      'ezbuy'
+    ].map(pth => ({
+      source: `/${pth}`,
+      destination: `/dpo/${pth}`,
+      permanent: true
+    }))
+
+    return redirectSapperToNextJSPaths.concat([
+      {
+        source: '/dpo/posb',
+        destination: '/dpo/dbs',
+        permanent: true
+      },
+      {
+        source: '/dpo/silkair',
+        destination: '/dpo/singapore-airlines',
+        permanent: true
+      },
+      {
+        source: '/dpo/youtube',
+        destination: '/dpo/google',
+        permanent: true
+      }
+    ])
   },
 }
